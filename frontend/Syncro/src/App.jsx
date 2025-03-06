f7b4ed43856fb04d2385f9d2725b960593f351a5
Lucas Bustabad <iesvda.lucrod@gmail.com>
2025-03-06 19:58:54 +0100

    Started with navBar

--- frontend/Syncro/src/App.jsx
+++ frontend/Syncro/src/App.jsx
@@ -2,28 +2,36 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css'
 
+//Controller imports
+
 //UI Component imports
 import Navigation from "./views/components/Navigations";
 import HomePage from './views/pages/HomePage';
 import NotFoundPage from './views/pages/NotFoundPage';
+import LoginPage from './views/pages/LoginPage';
+import UserContextProvider from './views/contexts/UserContext.jsx';
 
 function App() {
 
   return (
     <div>
-      <Navigation/>
-      <h1>APP COMPONENT</h1>
+      <UserContextProvider>
+        <Router>
 
-      
-      <Router>
-        <Routes>
+        <Navigation/>
+
+
+          <Routes>
+            
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+
+          </Routes>
+
           
-          <Route path="/" element={<HomePage />} />
-          <Route path="*" element={<NotFoundPage />} />
-
-        </Routes>
-      </Router>
-      
+        </Router>
+      </UserContextProvider>
     </div>
   )
 }
--- frontend/Syncro/src/views/components/Navigations.jsx
+++ frontend/Syncro/src/views/components/Navigations.jsx
@@ -1,14 +1,24 @@
+import { Link } from "react-router-dom";
+import Button from "./Button";
+
+import '../styles/Navigation.css';
+
 export default function Navigation() {
     return (
-        <div>
-            <h3>Navigation component</h3>
-            <span>Logo and</span>
-            <span>Dashboard</span>
-            <span></span>
-            <span></span>
-            ----
-            <span>Account</span>
+        <div className="Navigation">
+            <div className="menuOppener">
+                <Button>Open</Button>
+            </div>
+            
+            <div className="menuLinks">
+                <Button as={Link} to={'/'}>Home</Button>
+                <Button as={Link} to={'/profile'}>Profile</Button>
+                <Button as={Link}  to={'/contact'}>Contact</Button>
+            </div>
 
+            <div className="menuDashboards">
+
+            </div>
         </div>
     );
 };
