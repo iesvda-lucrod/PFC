5ecd8a835aa47f7d51bf5c9d5c088387a9ee7e34
Lucas Bustabad <iesvda.lucrod@gmail.com>
2025-04-04 10:24:18 +0200

    dotenv fix

--- backend/config.php
+++ backend/config.php
@@ -1,5 +1,5 @@
 <?php
-require __DIR__.'/../vendor/autoload.php'; // Load Composer packages
+require_once __DIR__.'/../vendor/autoload.php'; // Load Composer packages
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
