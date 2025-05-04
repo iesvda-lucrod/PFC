ed224c7947568ed51161835ba265ca5f44393b30
Lucas Bustabad <iesvda.lucrod@gmail.com>
2025-05-04 16:09:18 +0200

    RoomContext info fetching function

--- frontend/Syncro/src/contexts/RoomContext.jsx
+++ frontend/Syncro/src/contexts/RoomContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useState } from "react";
 import useSection from "../models/useSection";
 import useRoom from "../models/useRoom";
 import useTask from "../models/useTask";
@@ -21,25 +21,44 @@ export function RoomContextProvider(props) {
     const loadRoomInfo = async (roomId) => {
         console.log("Fetching room info...");
         const roomResponse = await roomModel.getRoomInfo(roomId);
-        setRoomInfo(roomResponse.data);
+
         console.log("Fething room sections...");
         const sectionResponse = await sectionModel.getRoomSections(roomId);
-        setSections(sectionResponse.data);
+        let sectionMap = new Map();
+        sectionResponse.data.forEach(section => {
+            sectionMap.set(section.id, section);
+        });
 
+        console.log("Fetching room tasks...");
         const sectionIds = sectionResponse.data.map((section) => section.id);
         const taskResponse = await taskModel.getSectionTasks(sectionIds);
-        //let orderedTasks = taskResponse.data.map+(map) => {}
 
-        console.log("Room information:", roomResponse.data, "Sections:",sectionResponse.data, "Tasks:", taskResponse.data);
+        console.log("Organizing tasks...");
+        let orderedTasks = new Map();
+        taskResponse.data.forEach((task) => {
+            if (!orderedTasks.has(task.section_id)) orderedTasks.set(task.section_id, []);
+            orderedTasks.get(task.section_id).push(task);
+        });
+
+        console.log("Assigning tasks to sections...");
+        sectionMap.forEach((value) => {
+            value.tasks = orderedTasks.get(value.id) || [];
+        });
+
+        setRoomInfo(roomResponse.data);
+        setSections(sectionMap);
+        setTasks(orderedTasks); //TODO Needed?
+
+        console.log("Room information:", roomResponse.data, "Sections:",sectionMap, "Tasks:", taskResponse.data);
     }
 
     return (
         <RoomContext.Provider
             value={{
                 loadRoomInfo,
-                room: {roomIsLoading, roomInfo, roomModel},
-                section: {sectionIsLoading, sections, sectionModel},
-                task: {taskIsLoading, tasks, taskModel}
+                room: {roomIsLoading, roomInfo, setRoomInfo, roomModel},
+                section: {sectionIsLoading, sections, setSections, sectionModel},
+                task: {taskIsLoading, tasks, setTasks, taskModel}
             }}
         >
             {children}
