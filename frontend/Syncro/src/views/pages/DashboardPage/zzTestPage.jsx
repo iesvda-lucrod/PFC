da9f0d2be08f8a253927d96655947af38d7c1440
Lucas Bustabad <iesvda.lucrod@gmail.com>
2025-05-14 22:18:45 +0200

    Added custom CSS variables

---
+++ frontend/Syncro/src/views/pages/DashboardPage/zzTestPage.jsx
@@ -0,0 +1,11 @@
+export default function zzTestPage() {
+
+    
+
+    return (
+        <div>
+            <input type="text" name="" id="" />
+            <button>Dubmit</button>
+        </div>
+    );
+}
