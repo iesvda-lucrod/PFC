257f20c673705e3c0bf4c3da88d8b4939d4b30d4
Lucas Bustabad <iesvda.lucrod@gmail.com>
2025-05-14 22:16:33 +0200

    Reformatted user information fetching

--- backend/services/DBAccess/UsersTable.php
+++ backend/services/DBAccess/UsersTable.php
@@ -4,20 +4,35 @@ require_once __DIR__ . '/DBConnection.php';
 class UsersTable extends DBConnection{ 
     public function __construct()
     {
-        parent::__construct("users", 'id, email, password, username');
+        parent::__construct("users", "id, email, username");
     }
 
-    public function getUserFromEmail($userData) {
-        $result = $this->selectByField('email', $userData['email']);
+    /**
+     * Get the user's information from their email
+     * @param mixed $userData An array that must contain an 'email' clause to extract the email from
+     */
+    public function getUserFromEmail($userEmail) {
+        $result = $this->selectByField('email', $userEmail);
         return isset($result[0]) ? $result[0] : null;
     }
-    public function getFullUserFromEmail($userData) {
+    /**
+     * INCLUDES SENSITIVE DATA,
+     * BE SURE TO UNSET THE FIELDS BEFORE INCLUDING THE RESULT IN A RESPONSE
+     * Get the user's information from their email.
+     * @param mixed $userData
+     * 
+     */
+    public function getUnprotectedUserFromEmail($userEmail) {
         $this->fields = '*';
-        $result = $this->getUserFromEmail($userData);
-        $this->fields = 'id, email, password, username';
+        $result = $this->getUserFromEmail($userEmail);
+        $this->fields = 'id, email, username';
         return $result;
     }
 
+    public function isUserEmailVerified($userEmail) {
+        $result = $this->getUnprotectedUserFromEmail($userEmail);
+        return $result['verified'];
+    }
 
     public function getUserCredentials($userData) {
         $this->fields = 'id, email, username, password';
