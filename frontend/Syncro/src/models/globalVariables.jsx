10d3e5f00eb9be906eb44007d0e7e8a0fbba07f7
Lucas Bustabad <iesvda.lucrod@gmail.com>
2025-05-30 19:02:47 +0200

    changed routes to reflect production

--- frontend/Syncro/src/models/globalVariables.jsx
+++ frontend/Syncro/src/models/globalVariables.jsx
@@ -1,4 +1,4 @@
 export const SERVER_URL = 'http://localhost/';
-export const PROFILE_PICTURES_DIRECTORY = SERVER_URL+'PFC/backend/assets/images/profile_pictures/';
+export const PROFILE_PICTURES_DIRECTORY = SERVER_URL+'Lucas_PFC/backend/assets/images/profile_pictures/';
 
-export const ENDPOINT_URL = 'http://localhost/PFC/backend/api/';
+export const ENDPOINT_URL = 'http://localhost/Lucas_PFC/backend/api/';
